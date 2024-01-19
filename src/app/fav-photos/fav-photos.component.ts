920a2f16bd89687938eecc3dedf97e0ccc4245bd
crystalchavez99 <redfire1205@hotmail.com>
2024-01-19 11:51:58 -0800

    added photos

--- src/app/fav-photos/fav-photos.component.ts
+++ src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'Photos';
+  image1 = 'https://m.media-amazon.com/images/I/81KGsbq8ekL._AC_UF1000,1000_QL80_.jpg';
+  image2 = 'https://www.impericon.com/432x624x90/media/catalog/product/p/p/pp35325_e4a6d2c6-bd24-44e1-b988-38e1031700e2.jpg';
+  image3 = 'https://cdn.vox-cdn.com/thumbor/1jaotrFmU5EcSqS18_AAVWEwgwA=/1400x1400/filters:format(png)/cdn.vox-cdn.com/uploads/chorus_asset/file/24380016/Legend_of_Zelda_Ocarina_of_Time_official_art.png';
 
   constructor() { }
 
