cd1291b96d7fd741eb28bf01063727e4ada6383b
crystalchavez99 <redfire1205@hotmail.com>
2024-01-21 22:25:24 -0800

    ngif and ngfor

--- src/app/chores-list/chores-list.component.ts
+++ src/app/chores-list/chores-list.component.ts
@@ -6,8 +6,10 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./chores-list.component.css']
 })
 export class ChoresListComponent implements OnInit {
-   chores = ['Empty dishwasher', 'Complete LaunchCode prep work', 'Buy groceries', 'Clean Bathroom'];
-   todoTitles = ["Yesterday's Chores", "Today's Chores", "Tomorrow's Chores"];
+  chores = ['Take out trash'];
+  finishedChores = ['Empty dishwasher', 'Complete LaunchCode prep work','Buy groceries', 'Walk Dpg!'];
+  todoTitles = ["Chores", "Done Chores"];
+  targetImage = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
 
    constructor() { }
 
